776ffe7c3d5feb6724b9cb8e01ce26110b6e0951
Arquitenens <norodnoastro@gmail.com>
2026-07-20 12:23:31 +0200

    update

--- scheduler.rs
+++ scheduler.rs
@@ -1,57 +1,30 @@
-use crate::builder::SchedulerBuilder;
-use crate::scheduler_config::{AnyHandler, Config};
-use crate::task::{AnyArg, TaskConsumer, TaskProducer, TypelessTask};
-use crate::worker::Worker;
-use std::any::{Any, TypeId};
+use crate::builder::{SchedulerBuilder, TypesIdx, MAX_SCHEDULERS};
+use crate::scheduler_config::Config;
+use crate::task::{Task, TypelessTask};
+use crate::worker::{SendPtr, Worker};
+use std::any::TypeId;
 use std::collections::HashMap;
-use std::env::Args;
-use std::marker::PhantomData;
-use std::mem::{ManuallyDrop, MaybeUninit};
-use std::ops::Range;
+use std::mem::MaybeUninit;
 use std::ptr;
 use std::ptr::null_mut;
-use std::sync::atomic::{AtomicPtr, AtomicU64, AtomicUsize};
+use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicU64, Ordering};
 use std::sync::{Arc, RwLock};
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::thread::JoinHandle;
+use std::time::Instant;
 
-
-
-pub(crate) const MAX_WORKERS_PER_SCHED: usize = 32;
+pub(crate) const MAX_WORKERS_PER_SCHED: usize = 64;
 
 pub struct Scheduler{
-    pub generic_schedulers: HashMap<TypeId, Vec<SubScheduler>>,
+    pub generic_schedulers: Vec<Box<SubScheduler>>,
     //type id here is for the Task/Function not for the scheduler
     pub reusable_tasks: Arc<HashMap<TypeId, usize>>,
     //usize is the element count
-    pub task_wrapper: ([Arc<RwLock<TypelessTask>>; MAX_WORKERS_PER_SCHED], usize),
-
-    pub queue_holder: [Option<(TaskProducer, TaskConsumer)>; MAX_WORKERS_PER_SCHED],
+    //TODO maybe wrap in RwLock for mutability on some arguments? removed for now, dont know how to implement
+    pub task_wrapper: ([*mut TypelessTask; MAX_WORKERS_PER_SCHED], usize),
 
     pub config: Config
 }
-
-
-pub struct SelfHandle<'a, T: Send>{
-    pub val: *const T,
-    //TODO dont know about ManuallyDrop
-    pub _phantom: PhantomData<&'a ManuallyDrop<T>>
-}
-impl<'a, T: Send> SelfHandle<'a, T> {
-
-    ///ref makes sure you have no exclusive references anymore
-    ///make sure it lives as long as T
-    //TODO prob redesign
-    pub unsafe fn new<'guard>(self_: &'a T) -> Self where 'a: 'guard {
-        Self{
-            val: ptr::from_ref(self_),
-            _phantom: PhantomData
-        }
-    }
-
-    fn call(&mut self){
-        //TODO probably not gonna use this
-    }
-}
-
 
 
 impl Scheduler {
@@ -59,7 +32,7 @@ impl Scheduler {
 
         return SchedulerBuilder{
             parent: ptr::null_mut(),
-            generic_schedulers: HashMap::default(),
+            generic_schedulers: Vec::default(),
             config,
             queue: MaybeUninit::uninit(),
             sub_scheduler_amount: 0,
@@ -68,91 +41,123 @@ impl Scheduler {
     }
 
     ///Get any
-    pub async fn any<'self_lt, 'other_lt, S, F, R, Args>(&'self_lt mut self, this: SelfHandle<'other_lt, S>, args: Vec<Box<dyn Any>>, exec: F)
-    where F: AnyHandler<Args, Output = R> + Send + 'static,
-          S: Send + 'static,
-          Args: AnyArg + 'static + Send,
+    pub fn any<'self_lt, 'other_lt, T, F>
+    (&'self_lt mut self, exec: F)
+    where F: FnMut() + Send + 'static,
+          T: 'static + TypesIdx,
           'self_lt: 'other_lt,
     {
 
+        if T::tid_idx() >= MAX_SCHEDULERS{
+            return;
+        }
+        let inner: &mut Box<SubScheduler> = &mut self.generic_schedulers[T::tid_idx()];
 
-        let matches = exec.matching_arguments(args.as_slice());
-        dbg!(matches);
+        //TODO non atomic way is SIMD masking 512bit reg using roughly vpmovmskb to turn a [bool; N] into a bitmask
+        let available_workers = inner.worker_state.load(Ordering::Acquire);
+        //println!("available workers: {:b}", available_workers);
+        if available_workers == 0 {
+            return;
+        }
 
-        let arg_len = args.len();
+        let available_idx = available_workers.trailing_zeros() as usize;
 
-
-        //let tid_of_task = &TypeId::of::<F>();
-        let inner = match self.generic_schedulers.get_mut(&TypeId::of::<Args>()){
-            Some(inner) => inner,
-            None => {
-                //dbg!(&TypeId::of::<Args>());
-                &mut Vec::new()
+        let mut temp = available_workers;
+        //TODO prefetch count
+        #[cfg(feature = "?")]
+        for _ in 0..4{
+            if temp == 0 {
+                break;
             }
+            let pre_idx = temp.trailing_zeros() as usize;
+            temp &= !(1u64 << pre_idx);
+            //availabe.workers[pre_idx].notify_one
+        }
+        inner.worker_state.fetch_and(!(1u64 << available_idx), Ordering::Release);
+
+        let tid_of_task = TypeId::of::<F>();
+
+        if let Some(idx) = self.reusable_tasks.get(&tid_of_task){
+            let cached_task = &mut unsafe {(*self.task_wrapper.0[*idx]).clone()};
+            unsafe {inner.producer[available_idx].swap(cached_task, Ordering::Release);};
+            std::sync::atomic::compiler_fence(Ordering::Release);
+            return;
         };
 
+        let raw_fn: *const F = Box::into_raw(Box::new(exec));
+        let task = &mut unsafe {TypelessTask::new::<F, F>(raw_fn)};
+        let amount = self.task_wrapper.1;
+        self.task_wrapper.0[amount % self.task_wrapper.0.len()] = &mut task.clone();
+        self.task_wrapper.1 += 1;
 
-        let selected = Scheduler::get_available_single(inner);
-
-        //TODO use task id to manage caching
-        //at the time of calling the producers should have already been successfully initialized, thus you can assume so
-        //TODO somehow turn F into TypelessTask
+        //TODO this is very weird... technically ub? but its logically sound and consistent
+        //TODO honestly I don't know how the cpu treats this, if its causing weird stuff just replace with atomic ptr + write
+        unsafe {inner.producer[available_idx].swap(task, Ordering::Release);};
+        //fence prob not needed?
+        std::sync::atomic::compiler_fence(Ordering::Release);
     }
 
-    fn args_match<T: AnyArg>(any_args: Vec<Box<dyn Any>>, args: T) -> bool{
-
-
-
-        for arg in any_args.iter(){
-
-        }
-        todo!();
-    }
-
-    fn get_available_single<'a>(of: &mut Vec<SubScheduler>) ->  Option<*mut SubScheduler> {
-
-        return None
-    }
 }
 
 pub struct SubScheduler{
     pub(crate) parent: AtomicPtr<Scheduler>,
     pub(crate) idx: usize,
-    pub(crate) reusable_task: Arc<RwLock<TypelessTask>>,
-    //not actually static, im just a liar
-    //though it will live as long as the Scheduler itself
-    pub(crate) producer: Arc<[TaskProducer]>,
-
+    pub(crate) producer: [AtomicPtr<TypelessTask>; MAX_WORKERS_PER_SCHED],
+    pub(crate) thread_stop: [AtomicBool; MAX_WORKERS_PER_SCHED],
+    pub(crate) handles: Vec<JoinHandle<()>>,
 
     //TODO this might not even need to be atomic at all!
-    //TODO it is shared and will cause a race condition but its not actually an issue?
+    //TODO it is shared and will cause a race condition but its not actually an issue
     pub(crate) worker_state: Arc<AtomicU64>,
 }
 
-impl SubScheduler {
-    pub fn new(queue: &mut MaybeUninit<[Option<(TaskProducer, TaskConsumer)>; MAX_WORKERS_PER_SCHED]>, workers: usize, idx: usize) -> SubScheduler {
-
-
-        let worker_state = Arc::new(AtomicUsize::new(0));
-        
-        let mut producers = Vec::new();
-        
-        for worker in 0..workers{
-            let (tx, rx) = unsafe {queue.assume_init_mut()[worker].take().unwrap()};
-            producers.push(tx);
-            Worker::new(worker_state.clone(), rx);
+impl Drop for SubScheduler {
+    fn drop(&mut self) {
+        for (idx, h) in self.handles.drain(..).enumerate(){
+            self.thread_stop[idx].store(true, Ordering::Release);
+            h.join().unwrap();
         }
+    }
+}
 
-        return SubScheduler{
+impl SubScheduler {
+    pub fn new(workers: usize, idx: usize) -> Box<SubScheduler> {
+
+
+        let worker_state = Arc::new(AtomicU64::new((1 << workers as u64) - 1));
+        //println!("worker_state: {}", worker_state.load(Ordering::Acquire));
+        
+        let producers = [const {AtomicPtr::new(null_mut::<TypelessTask>())}; MAX_WORKERS_PER_SCHED];
+        let handles = Vec::with_capacity(MAX_WORKERS_PER_SCHED);
+        let thread_stop = [const {AtomicBool::new(false)}; MAX_WORKERS_PER_SCHED];
+
+        let mut incomplete_sub = Box::new(SubScheduler{
             parent: AtomicPtr::new(null_mut()),
             idx,
             //usize::MAX == all workers available
-            worker_state: Arc::new(AtomicU64::new(u64::MAX)),
-            reusable_task: Arc::new(
-                RwLock::new(
-                    TypelessTask::default()
-                )),
-            producer: producers.into(),
-        };
+            worker_state: worker_state.clone(),
+            producer: producers,
+            thread_stop,
+            handles,
+        });
+
+        for worker in 0..workers{
+
+            //Tx is not valid yet, its important that its None
+            let sig = SendPtr(ptr::from_ref(&incomplete_sub.thread_stop[worker]) as *mut _);
+
+            let null: *mut TypelessTask = null_mut();
+
+            let w = Worker::new(worker_state.clone(), AtomicPtr::from(incomplete_sub.producer[worker].load(Acquire)), worker, sig);
+            let handle = std::thread::spawn(move || {
+                w.execute()
+            });
+            incomplete_sub.handles.push(handle);
+        }
+
+
+        incomplete_sub.worker_state = worker_state;
+
+        return incomplete_sub
     }
 }

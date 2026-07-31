64d51fcf5076714ed1f260056034d54c66ea69a5
Arquitenens <norodnoastro@gmail.com>
2026-07-31 15:48:31 +0200

    Refactor SchedulerBuilder to support thread overwrite

--- builder.rs
+++ builder.rs
@@ -1,6 +1,5 @@
 
-use crate::Any;
-use std::any::TypeId;
+use std::any::{Any, TypeId};
 use std::cell::{OnceCell, UnsafeCell};
 use std::collections::HashMap;
 use std::hash::Hash;
@@ -12,7 +11,7 @@ use std::ptr;
 use std::ptr::{addr_of_mut, null_mut};
 use std::sync::{Arc, OnceLock, RwLock};
 use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
-use crate::scheduler::MAX_WORKERS_PER_SCHED;
+use crate::scheduler::{FIDLookUp, MAX_WORKERS_PER_SCHED};
 pub(crate) use crate::scheduler::Scheduler;
 pub(crate) use crate::scheduler::SubScheduler;
 use crate::scheduler_config::Config;
@@ -21,34 +20,40 @@ use crate::worker::SendPtr;
 
 //TODO align struct fields
 pub struct SchedulerBuilder{
-    pub(crate) parent: *mut Scheduler,
     pub(crate) generic_schedulers: Vec<Box<SubScheduler>>,
     pub(crate) config: Config,
+    pub(crate) total: usize,
     pub(crate) sub_scheduler_amount: usize,
-    pub(crate) queue: MaybeUninit<[Option<(*mut Option<TypelessTask>, *mut Option<TypelessTask>)>; MAX_WORKERS_PER_SCHED]>,
     pub(crate) inner: MaybeUninit<Scheduler>
 }
 
 pub const MAX_SCHEDULERS: usize = 16;
-static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
-#[derive(Clone, Copy)]
-pub struct Idx(pub(crate) fn() -> usize);
-pub static ADD_TYPE: Idx = Idx(||{
-    static ID: OnceLock<usize> = OnceLock::new();
-    *ID.get_or_init(|| NEXT_ID.fetch_add(1, Ordering::Relaxed))
-});
 pub trait TypesIdx{
     fn tid_idx() -> usize;
 }
 
 
 
-
+pub enum ThreadAmount{
+    Overwrite(usize),
+    Default
+}
 impl SchedulerBuilder {
-    pub fn add_scheduler<T: Any>(mut self) -> Self {
-        //TODO increments even for the same type, i dont know if that's good or if i should do a check
+    pub fn add_scheduler<T: Any>(mut self, thread_overwrite: ThreadAmount) -> Self {
+        let mut amount = self.config.threads_per_sub_sched;
+        let threads_for_this = match thread_overwrite {
+            ThreadAmount::Overwrite(x) => {
+                amount = x;
+                self.config.threads_per_sub_sched + x
+            },
+            ThreadAmount::Default => self.config.threads_per_sub_sched,
+        };
 
-        self.generic_schedulers.push(SubScheduler::new(self.config.threads_per_sub_sched, self.sub_scheduler_amount));
+        let offset = self.total;
+        self.total += threads_for_this;
+
+
+        self.generic_schedulers.push(SubScheduler::new(self.config.threads_per_sub_sched, self.sub_scheduler_amount, offset, amount));
 
         self.sub_scheduler_amount += 1;
         return self
@@ -57,9 +62,11 @@ impl SchedulerBuilder {
     pub fn apply(mut self) -> Scheduler{
         
         self.inner = MaybeUninit::new(Scheduler{
-            config: self.config,
             generic_schedulers: self.generic_schedulers,
-            reusable_tasks: Arc::new(HashMap::new()),
+            reusable_tasks: FIDLookUp {
+                entries: const {[(TypeId::of::<i32>(), 0); 16]},
+                cursor: 0,
+            },
             task_wrapper: ([const {null_mut::<TypelessTask>()}; 64], 0),
         });
         return unsafe {self.inner.assume_init()}
